1fff659b94788f65adf633ca3f5905ef19739fe4
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2025-04-19 21:06:11 +0200

    style: fix linter issues

--- internal/du/du.go
+++ internal/du/du.go
@@ -7,7 +7,14 @@ import "syscall"
 
 func Available(path string) uint64 {
 	var stat syscall.Statfs_t
-	syscall.Statfs(path, &stat)
+	err := syscall.Statfs(path, &stat)
+	if err != nil {
+		return 0
+	}
+
+	if stat.Bsize < 0 {
+		return 0
+	}
 
 	return stat.Bavail * uint64(stat.Bsize)
 }
